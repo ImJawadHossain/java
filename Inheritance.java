b695a58d43f4252382f49659ad48bf5551c64e82
Md Jawad Hossain <jawadsgs3@gmail.com>
2023-05-04 04:16:43 -0500

    Update Inheritance.java

--- Inheritance.java
+++ Inheritance.java
@@ -74,3 +74,91 @@ public static void main(String[] args) {
 }
 
 
+
+
+
+
+// Multilevel Inheritance
+
+package jawad;
+
+//superclass
+class Animal {
+void eat() {
+ System.out.println("Animal is eating");
+}
+}
+
+//subclass of Animal
+class Dog extends Animal {
+void bark() {
+ System.out.println("Dog is barking");
+}
+}
+
+//subclass of Dog
+class Poodle extends Dog {
+void play() {
+ System.out.println("Poodle is playing");
+}
+}
+
+//main method
+public class Main {
+public static void main(String[] args) {
+ Poodle myPoodle = new Poodle();
+ myPoodle.eat();   // inherited from Animal class
+ myPoodle.bark();  // inherited from Dog class
+ myPoodle.play();
+}
+}
+
+
+
+
+
+
+
+
+
+// Hierarchical Inheritance
+
+package jawad;
+
+//superclass
+class Animal {
+void eat() {
+ System.out.println("Animal is eating");
+}
+}
+
+//subclass of Animal
+class Dog extends Animal {
+void bark() {
+ System.out.println("Dog is barking");
+}
+}
+
+//subclass of Animal
+class Cat extends Animal {
+void meow() {
+ System.out.println("Cat is meowing");
+}
+}
+
+//main method
+public class Main {
+public static void main(String[] args) {
+ Dog myDog = new Dog();
+ myDog.eat();   // inherited from Animal class
+ myDog.bark();
+ 
+ Cat myCat = new Cat();
+ myCat.eat();   // inherited from Animal class
+ myCat.meow();
+}
+}
+
+
+
+
